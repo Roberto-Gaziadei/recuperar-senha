cfe0419f5cd6251bfdfa4fd2eb95b7747be5627c
Roberto_Graziadei <roberto.2022315930@aluno.iffar.edu.br>
2024-06-24 17:05:42 -0300

    .

--- form-cadastro.php
+++ form-cadastro.php
@@ -1,16 +1,19 @@
 <!DOCTYPE html>
-<html lang="pt_BR">
+<html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de Usuário</title>
 </head>
+
 <body>
     <form action="cadastrar.php" method="post">
-        <label>Nome: <input type="text" nome="nome"></label><br>
+        <label>Nome: <input type="text" name="nome"></label><br>
         <label>Email: <input type="email" name="email"></label><br>
         <label>Senha: <input type="password" name="senha"></label><br>
-        <input type="submit" value="Cadastrar"> 
+        <input type="submit" value="Cadastrar-se">
     </form>
 </body>
+
 </html>
---
+++ nova-senha.php
@@ -0,0 +1,11 @@
+<?php
+// verificar o email
+// verificar o token
+
+
+$email = $_GET['email'];
+$token = $_GET['token'];
+
+
+
+?>
